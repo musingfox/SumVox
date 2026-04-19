b579e86ff8c42f87fc56e23ffc10e36085989fcb
musingfox <nick12703990@gmail.com>
2026-04-20 01:45:00 +0800

    chore: bump version to 1.5.0

--- homebrew/sumvox.rb
+++ homebrew/sumvox.rb
@@ -1,25 +1,25 @@
 class Sumvox < Formula
   desc "Intelligent voice notifications for AI coding tools"
   homepage "https://github.com/musingfox/sumvox"
-  version "1.4.1"
+  version "1.5.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/musingfox/sumvox/releases/download/v1.4.1/sumvox-macos-aarch64.tar.gz"
+      url "https://github.com/musingfox/sumvox/releases/download/v1.5.0/sumvox-macos-aarch64.tar.gz"
       sha256 "73a699320c4a0aa3f6dab4c88beae3a9ed303efa42fc03877e5b988eb0f53a6b"
     else
-      url "https://github.com/musingfox/sumvox/releases/download/v1.4.1/sumvox-macos-x86_64.tar.gz"
+      url "https://github.com/musingfox/sumvox/releases/download/v1.5.0/sumvox-macos-x86_64.tar.gz"
       sha256 "3ce69ce8d11aab2cb419ec2788f704cecda93974431a36ab5d0b2f907bd77319"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/musingfox/sumvox/releases/download/v1.4.1/sumvox-linux-aarch64.tar.gz"
+      url "https://github.com/musingfox/sumvox/releases/download/v1.5.0/sumvox-linux-aarch64.tar.gz"
       sha256 "2fdf2c9136e1605725092d435f163db87e42adb0cdf7ba999b9f98eae84a257c"
     else
-      url "https://github.com/musingfox/sumvox/releases/download/v1.4.1/sumvox-linux-x86_64.tar.gz"
+      url "https://github.com/musingfox/sumvox/releases/download/v1.5.0/sumvox-linux-x86_64.tar.gz"
       sha256 "956b924926d123c4feb4ad10ee9f4461422c6b79b5ac1378cfd05725a845d77e"
     end
   end
